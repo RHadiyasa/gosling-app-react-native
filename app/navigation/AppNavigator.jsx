3f9affdd9668feabd32188a5440ec4372a8c05aa
Rafi Hadiyasa <rafihadiyasa32@gmail.com>
2024-08-20 22:57:14 +0700

    Fix Redux

--- app/navigation/AppNavigator.jsx
+++ app/navigation/AppNavigator.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WelcomeScreen from "../screens/WelcomeScreen";
 import RegisterScreen from "../screens/RegisterScreen";
 import LoginScreen from "../screens/LoginScreen";
 import TabNavigator from "./TabNavigator";
 import { useSelector } from "react-redux";
+import { Text, View } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
+  const [loading, setLoading] = useState(true);
   const userSelector = useSelector((state) => state.loggedInUser);
+  console.log(userSelector.loggedInUser.id);
+
+  useEffect(() => {
+    // Simulasi fetch data
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  });
+
+  if (loading) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
 
   return (
-    <Stack.Navigator initialRouteName={userSelector.loggedInUser ? "Main" : "Welcome"}>
+    <Stack.Navigator
+      initialRouteName={userSelector.loggedInUser.id ? "Main" : "Welcome"}
+    >
       <Stack.Screen
         name="Welcome"
         component={WelcomeScreen}
